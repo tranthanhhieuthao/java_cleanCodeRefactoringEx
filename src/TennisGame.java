446c976d08ecaa0c66d7909d07a379688fcc1cb0
HieuThao <you@example.com>
2019-06-12 13:56:32 +0700

    bt

--- src/TennisGame.java
+++ src/TennisGame.java
@@ -1,11 +1,11 @@
 public class TennisGame {
 
-    public static String getScore(String player1Name, String player2Name, int m_score1, int m_score2) {
+    public static String getScore(String player1Name, String player2Name, int scorePlayerOne, int scorePlayerTwo) {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
+        int tempScore = 0;
+        if (scorePlayerOne == scorePlayerTwo)
         {
-            switch (m_score1)
+            switch (scorePlayerOne)
             {
                 case 0:
                     score = "Love-All";
@@ -25,9 +25,9 @@ public class TennisGame {
 
             }
         }
-        else if (m_score1>=4 || m_score2>=4)
+        else if (scorePlayerOne >= 4 || scorePlayerTwo >= 4)
         {
-            int minusResult = m_score1-m_score2;
+            int minusResult = scorePlayerOne-scorePlayerTwo;
             if (minusResult==1) score ="Advantage player1";
             else if (minusResult ==-1) score ="Advantage player2";
             else if (minusResult>=2) score = "Win for player1";
@@ -37,21 +37,22 @@ public class TennisGame {
         {
             for (int i=1; i<3; i++)
             {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
+                if (i==1) tempScore = scorePlayerOne;
+                else { score += "-"; tempScore = scorePlayerTwo;}
                 switch(tempScore)
                 {
                     case 0:
-                        score+="Love";
+                        score += "Love";
                         break;
+
                     case 1:
-                        score+="Fifteen";
+                        score += "Fifteen";
                         break;
                     case 2:
-                        score+="Thirty";
+                        score += "Thirty";
                         break;
                     case 3:
-                        score+="Forty";
+                        score += "Forty";
                         break;
                 }
             }
